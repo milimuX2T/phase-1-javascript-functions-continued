ed68f33b2c523ffc0d95176de20a1e2092eb7a19
Mathews <mathews.milimu@student.moringaschool.com>
2022-08-19 11:16:33 +0300

    Lab done

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// code your solution here
+function saturdayFun(arg) {
+ if  ( arg !== undefined){
+    return `This Saturday, I want to ${arg}!`
+ }
+ return "This Saturday, I want to roller-skate!" ;  
+}
+
+saturdayFun()
+
+const mondayWork = function (arg) {
+    if (arg !== undefined){
+        return `This Monday, I will ${arg}.`
+
+    }
+    return "This Monday, I will go to the office.";
+  };
+
+  mondayWork()
+
+
+  //function wrapAdjective (arg){
+    
+   // const innerFunction = function (emphatic= "*a hard worker*!"){
+        //return `You are ${emphatic}`
+   // }
+   // return innerFunction
+
+    //}
+     
+    function wrapAdjective(arg){
+        if(arg=="*"){
+            const innerFunciton = function(adj){
+                return "You are *" + adj +"*!";
+            }
+            return innerFunciton;
+        }
+        if(arg=="||"){
+            const innerFunciton = function(adj){
+                return "You are ||" + adj +"||!";
+            }
+            return innerFunciton;
+        }
+    }
+    
+    
+    wrapAdjective()()
+
+//
+
+
